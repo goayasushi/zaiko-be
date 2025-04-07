61ed24ec90874982aacc1d175dd26bb475c0d991
goayasushi <goayasushi@gmail.com>
2025-04-07 09:51:40 +0900

    認証ユーザーの情報を返却するapi

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .views import UserInfoView
 
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("info/", UserInfoView.as_view(), name="user_info"),
 ]
