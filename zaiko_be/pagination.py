3b34744ace628e30be0bb182e3943e012c3650d5
goayasushi <goayasushi@gmail.com>
2025-04-12 17:43:53 +0900

    ページネーションのレスポンスにページサイズも追加

--- zaiko_be/pagination.py
+++ zaiko_be/pagination.py
@@ -6,11 +6,10 @@ class CustomPagination(PageNumberPagination):
     """
     カスタムページネーションクラス
 
-    ページネーションのレスポンスに現在のページ番号を追加します。
-    また、ページサイズをクエリパラメータで変更できるようにします。
+    ページネーションのレスポンスに現在のページ番号と固定のページサイズを追加します。
     """
 
-    page_size = 20  # デフォルトは20件/ページ
+    page_size = 20  # 固定のページサイズ（20件/ページ）
 
     def get_paginated_response(self, data):
         """
@@ -19,8 +18,9 @@ class CustomPagination(PageNumberPagination):
         return Response(
             {
                 "count": self.page.paginator.count,  # 総件数
-                "total_pages": self.page.paginator.num_pages,  # 総ページ数も追加
+                "total_pages": self.page.paginator.num_pages,  # 総ページ数
                 "current": self.page.number,  # 現在のページ番号
+                "page_size": self.page_size,  # 固定のページサイズ
                 "next": self.get_next_link(),
                 "previous": self.get_previous_link(),
                 "results": data,
